61be40ade439f3965c29d706f30a45fdd9fcf309
vanessahuhn <vanessa.huhn@gmail.com>
2018-11-07 13:55:12 +0100

    fin projet

---
+++ src/app/daily-link-list/daily-link-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Link } from '../model/link';
+import { DataserviceService } from '../services/dataservice.service';
+
+@Component({
+  selector: 'app-daily-link-list',
+  templateUrl: './daily-link-list.component.html',
+  styleUrls: ['./daily-link-list.component.css']
+})
+export class DailyLinkListComponent implements OnInit {
+
+  dailyLinks : Link[];
+
+  constructor( private dataService : DataserviceService) { }
+
+  ngOnInit() {
+    this.dataService.getDailyLinks().subscribe( links => this.dailyLinks = links);
+  }
+
+}
